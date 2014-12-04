c734499f64ac62855802a1342e4623b0b7e5d18d
DirtyJerz <fluidflow00@gmail.com>
2014-12-03 22:44:35 -0500

    leankernel extended

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,7 @@
 export ARCH=arm
 #AOSP
 export CROSS_COMPILE=arm-eabi-
-export PATH=/home/sysop/Android/WORKING_DIRECTORY/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:$PATH
+export PATH=/home/sysop/Android/aosp/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin:$PATH
 #Linaro
 #export CROSS_COMPILE=arm-linux-androideabi-
 #export PATH=/home/sysop/Android/WORKING_DIRECOTRY/prebuilts/gcc/linux-x86/arm/android-toolchain-eabi/bin:$PATH
